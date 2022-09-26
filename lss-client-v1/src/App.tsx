44295743267ab32a67e42b11fe8e3c19bdbf30d8
Parker Hudson <gphudson97@hotmail.com>
2022-09-26 05:01:27 -0400

    ResponseView component base implementation

--- lss-client-v1/src/App.tsx
+++ lss-client-v1/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./css/App.css";
+import ResponseView from "./components/responseView";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -18,7 +19,7 @@ function App() {
 			<h1>Vite + React</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}!
+					Count is {count}
 				</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
@@ -27,6 +28,7 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
+			<ResponseView></ResponseView>
 		</div>
 	);
 }
