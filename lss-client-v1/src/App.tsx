6277b72cfa06cc854765856993fcbf67b235e568
Carson Blin <carsonb551@gmail.com>
2022-09-26 01:40:38 -0400

    Admin Creation User Story

--- lss-client-v1/src/App.tsx
+++ lss-client-v1/src/App.tsx
@@ -1,34 +1,33 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+
 import "./css/App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    alert("Admin Account created!");
+  };
 
-	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}!
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>HabiJax Admin Dashboard </h1>
+      <br />
+      <h2>Create Admin Account</h2>
+      <form onSubmit={submitHandler}>
+        Username:
+        <input type="text" placeholder="Username" required />
+        <div className="col-md-6">
+          Password:
+          <input type="password" placeholder="Password" required />
+        </div>
+        <div>
+          Confirm Password:
+          <input type="password" placeholder="Confirm Password" required />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
