cfc12cbfd03004165c4ae02da21c5d38183b4a9a
Carson Blin <carsonb551@gmail.com>
2022-11-10 15:08:33 -0500

    Login Functionality

--- lss-client-v1/src/App.tsx
+++ lss-client-v1/src/App.tsx
@@ -1,31 +1,49 @@
 import { useState } from "react";
-
+import LoginForm from "./components/LoginForm";
 import "./css/App.css";
 
 function App() {
-  const submitHandler = (e) => {
-    e.preventDefault();
-    alert("Admin Account created!");
+  const adminLogin = {
+    username: "n01433452@unf.edu",
+    password: "pass",
+  };
+
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+
+  const Logout = () => {
+    console.log("Logged out successfully!");
+    setUser({ username: "", password: "" });
+  };
+
+  const Login = (details: any) => {
+    console.log(details);
+    if (
+      details.username == adminLogin.username &&
+      details.password == adminLogin.password
+    ) {
+      console.log("Logged In!");
+      setUser({
+        username: details.username,
+        password: details.password,
+      });
+    } else {
+      console.log("Incorrect Information. Please try again.");
+      setError("Incorrect Information. Please try again.");
+    }
   };
 
   return (
     <div className="App">
-      <h1>HabiJax Admin Dashboard </h1>
-      <br />
-      <h2>Create Admin Account</h2>
-      <form onSubmit={submitHandler}>
-        Username:
-        <input type="text" placeholder="Username" required />
-        <div className="col-md-6">
-          Password:
-          <input type="password" placeholder="Password" required />
-        </div>
-        <div>
-          Confirm Password:
-          <input type="password" placeholder="Confirm Password" required />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      {user.username != "" ? (
+        <>
+          <h1>Welcome to the Admin Dashboard!</h1>
+          <button onClick={Logout}>Logout</button>
+          <button>Create Account</button>
+        </>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
 }
---
+++ lss-client-v1/src/components/LoginForm.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function LoginForm({ Login, error }) {
+  const [details, setDetails] = useState({ username: "", password: "" });
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    Login(details);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <h2>Login</h2>
+      <h3>Please input information to login to survey system</h3>
+      {error != "" ? <div className="error">{error}</div> : ""}
+      Username:
+      <input
+        type="text"
+        id="userName"
+        placeholder="Username"
+        required
+        onChange={(e) => setDetails({ ...details, username: e.target.value })}
+        value={details.username}
+      />
+      <br></br>
+      <br></br>
+      Password:
+      <input
+        type="password"
+        id="password"
+        placeholder="Password"
+        required
+        onChange={(e) => setDetails({ ...details, password: e.target.value })}
+        value={details.password}
+      />
+      <br></br>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+export default LoginForm;
