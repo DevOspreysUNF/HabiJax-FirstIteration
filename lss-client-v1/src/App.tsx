7fe6203107b4203ca0e09895cf010c5aa683f0a1
Parker Hudson <gphudson97@hotmail.com>
2022-11-10 18:02:23 -0500

    docker compose

--- lss-client-v1/src/App.tsx
+++ lss-client-v1/src/App.tsx
@@ -3,31 +3,31 @@ import { useState } from "react";
 import "./css/App.css";
 
 function App() {
-  const submitHandler = (e) => {
-    e.preventDefault();
-    alert("Admin Account created!");
-  };
+	const submitHandler = (e: any) => {
+		e.preventDefault();
+		alert("Admin Account created!");
+	};
 
-  return (
-    <div className="App">
-      <h1>HabiJax Admin Dashboard </h1>
-      <br />
-      <h2>Create Admin Account</h2>
-      <form onSubmit={submitHandler}>
-        Username:
-        <input type="text" placeholder="Username" required />
-        <div className="col-md-6">
-          Password:
-          <input type="password" placeholder="Password" required />
-        </div>
-        <div>
-          Confirm Password:
-          <input type="password" placeholder="Confirm Password" required />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>HabiJax Admin Dashboard </h1>
+			<br />
+			<h2>Create Admin Account</h2>
+			<form onSubmit={submitHandler}>
+				Username:
+				<input type="text" placeholder="Username" required />
+				<div className="col-md-6">
+					Password:
+					<input type="password" placeholder="Passw ord" required />
+				</div>
+				<div>
+					Confirm Password:
+					<input type="password" placeholder="Confirm Password" required />
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
