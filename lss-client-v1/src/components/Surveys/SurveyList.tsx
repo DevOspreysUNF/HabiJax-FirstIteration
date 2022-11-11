1443ba02d49aed57bb4bcdeb4d73d8e2046f1581
Enrique Hernandez <enriqueh8588@gmail.com>
2022-11-11 12:10:44 -0500

    Added Delete function

--- lss-client-v1/src/components/Surveys/SurveyList.tsx
+++ lss-client-v1/src/components/Surveys/SurveyList.tsx
@@ -2,10 +2,19 @@ import react, { useState, useEffect } from 'react';
 
 const SurveyList = () => {
     const [survey, setSurvey] = useState([]);
+    const [surveyResponse, setSurveyResponse] = useState([]);
+    //console.log(survey);
+    //console.log(surveyResponse);
+
+    
+
 
     useEffect(() => {
         surveys();
-        // console.log(survey);
+        //deleteSurvey();
+        getResponse();
+        //console.log(survey);
+        //console.log(surveyResponse);
     }, [])
 
     // this will be called twice in development mode due to StrictMode re-rendering (leave it)
@@ -15,12 +24,13 @@ const SurveyList = () => {
             "method": "GET",
                 "headers": {
                     "Content-Type": "application/json",
-                    "X-API-TOKEN": "PUT-YOUR-TOKEN-HERE"
+                    "X-API-TOKEN": "aMCZkoJ23O0fcIAcLmkWITxXdxJqItLxeDIVRKKP"
                 }
             })
         // if(!response.ok) {
         //     throw new Error(`HTTP error! status: ${response.status}`);
         // }
+        
 
         setSurvey(await response.json().then(json => json.result.elements));
 
@@ -31,15 +41,59 @@ const SurveyList = () => {
         
     }
 
+    const deleteSurvey = async (surveyId:any) => {
+        try {
+            const response = await fetch("https://yul1.qualtrics.com/API/v3/surveys/" + surveyId, {
+            "method": "DELETE",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "X-API-TOKEN": "aMCZkoJ23O0fcIAcLmkWITxXdxJqItLxeDIVRKKP"
+                }
+            })
+
+        } catch(error) {
+            console.error();
+        }
+        
+          
+    }
+    const getResponse = async () => {
+        try {
+            const response = await fetch("https://yul1.qualtrics.com/API/v3/surveys/SV_5bEwX7qviWVF7FA", {
+            "method": "GET",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "X-API-TOKEN": "aMCZkoJ23O0fcIAcLmkWITxXdxJqItLxeDIVRKKP"
+                }
+            })
+
+        setSurveyResponse(await response.json().then(json => json.result));
+            
+            
+        } catch(error) {
+            console.error();
+        }
+        return surveyResponse;
+        
+    }
+   
+ 
     return(
         <>
             <h1>Surveys</h1>
-            <button>+ Create New Survey</button>
+            <form action="https://unf.co1.qualtrics.com/app/catalog/projects" target="_blank"><button>+ Create New Survey</button></form>
             <ul>
-                {survey.map((data:any) => { // :any temporary fix
-                    return(<li key={data.id}>{data.name}</li>)
+                {survey.map((data:any) => { // :any temporary fix 
+                    return(<li key={data.id}  /*onLoad={() => getResponse()}*/>
+                        <button onClick={() => deleteSurvey(data.id)}>Delete</button>
+                        {data.name}    {data.id}     {data.auditable}</li>)
                 })}
+                {/* {surveyResponse.map((data:any) => { // :any temporary fix
+                    return(<li key={data.id}  onLoad={() => getResponse(data.id)}>
+                        {data.name}    {data.id}     {data.auditable}</li>)
+                })} */}
             </ul>
+
         </>
     );
 }
