b8dae2c58c242174bf96481af3549c84d838389a
mirandaweathers <mirandaweathers@gmail.com>
2022-12-07 14:11:55 -0500

    remove placeholder text

--- lss-client-v1/src/components/Layout/Footer.tsx
+++ lss-client-v1/src/components/Layout/Footer.tsx
@@ -4,7 +4,6 @@ import NavLinks from './NavLinks';
 const Footer = () => {
     return(
         <footer>
-            <h1>footer placeholder!</h1>
             <ul className="footerul">
                 <NavLinks />
             </ul>
