6749516fc80fa8838da71771146329c68ab7e463
mirandaweathers <mirandaweathers@gmail.com>
2022-12-07 19:32:32 -0500

    get individual survey

--- lss-client-v1/src/services/SurveyService.ts
+++ lss-client-v1/src/services/SurveyService.ts
@@ -6,10 +6,22 @@ export default {
                     "Content-Type": "application/json",
                     "X-API-TOKEN": "aMCZkoJ23O0fcIAcLmkWITxXdxJqItLxeDIVRKKP"
                 }
-            })
+            }
+        )
 
             // if(!response.ok) {
             //     throw new Error(`HTTP error! status: ${response.status}`);
             // }
+    },
+
+    GetSurvey: (id:string) => {
+        return fetch(`https://c01.qualtrics.com/API/v3/surveys/${id}`, {
+            "method": "GET",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "X-API-TOKEN": "aMCZkoJ23O0fcIAcLmkWITxXdxJqItLxeDIVRKKP"
+                }
+            }
+        )
     }
 }
