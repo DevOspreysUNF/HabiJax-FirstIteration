0dd5be8b6edbbe96b85ab0788bb51852929b3591
Parker Hudson <gphudson97@hotmail.com>
2022-12-09 15:12:52 -0500

    Added export route

--- lss-client-v1/src/App.tsx
+++ lss-client-v1/src/App.tsx
@@ -7,31 +7,29 @@ import InputQuestion from "./components/InputQuestion";
 import "./css/App.css";
 import SurveyList from "./components/Surveys/SurveyList";
 import ResponseView from "./components/ResponseView";
-import SurveyDashboard from "./components/SurveyDashboard";
+import SurveyDashboard from "./components/Dashboard.tsx/SurveyDashboard";
 import Login from "./components/Login";
 import SurveyDetail from "./components/Surveys/SurveyDetail";
 import QuestionList from "./components/QuestionBank/QuestionList";
+import ExportData from "./components/dataExport/ExportData";
 
 function App() {
-
-
-  return (
-    <div className="App">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/surveys" element={<SurveyList />} />
-          <Route path="/questions" element={<QuestionList />} />
-          <Route path="/account" element={<Login />} />
-          <Route path="/survey-detail/:surveyId" element={<SurveyDetail />} />
-        </Routes>
-      </main>
+	return (
+		<div className="App">
+			<Navbar />
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/surveys" element={<SurveyList />} />
+					<Route path="/questions" element={<QuestionList />} />
+					<Route path="/account" element={<Login />} />
+					<Route path="/survey-detail/:surveyId" element={<SurveyDetail />} />
+					<Route path="/export" element={<ExportData />} />
+				</Routes>
+			</main>
 			<Footer />
-      
-    </div>
-  );
-
+		</div>
+	);
 }
 
 export default App;
