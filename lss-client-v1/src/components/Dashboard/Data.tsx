d26e7efbf03f327b21c6c897ea19c1cb7efd25a2
Parker Hudson <gphudson97@hotmail.com>
2022-12-09 19:16:38 -0500

    Data for graph

---
+++ lss-client-v1/src/components/Dashboard/Data.tsx
@@ -0,0 +1,27 @@
+export const Data = [
+	{
+		id: 1,
+		year: "May",
+		userGain: 2500,
+	},
+	{
+		id: 2,
+		year: "June",
+		userGain: 1350,
+	},
+	{
+		id: 3,
+		year: "July",
+		userGain: 4322,
+	},
+	{
+		id: 4,
+		year: "August",
+		userGain: 2244,
+	},
+	{
+		id: 5,
+		year: "September",
+		userGain: 5000,
+	},
+];
