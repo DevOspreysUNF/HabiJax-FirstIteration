c786379edd18a795961b3f6f14f708f32c674683
Parker Hudson <gphudson97@hotmail.com>
2022-12-09 19:17:29 -0500

    Added placeholder for contact page

---
+++ lss-client-v1/src/components/Contacts/ContactsDashboard.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import DataExportService from "../../services/DataExportService";
+import { useParams, useLocation } from "react-router-dom";
+
+const ContactsDashboard = (props: any) => {
+	return (
+		<>
+			<h2>Client Dashboard Placeholder</h2>
+		</>
+	);
+};
+
+export default ContactsDashboard;
