7c654282c2b52be033d2f7718147fecd9969425f
Carson Blin <carsonb551@gmail.com>
2022-12-13 13:17:43 -0500

    display account information

--- lss-client-v1/src/components/Login.tsx
+++ lss-client-v1/src/components/Login.tsx
@@ -4,16 +4,23 @@ import SurveyDashboard from "./SurveyDashboard";
 
 function Login() {
   const adminLogin = {
-    username: "admin",
+    username: "n01433452@unf.edu",
     password: "pass",
+    firstName: "Carson",
+    lastName: "Blin",
   };
 
-  const [user, setUser] = useState({ username: "", password: "" });
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
   const [error, setError] = useState("");
 
   const Logout = () => {
     console.log("Logged out successfully!");
-    setUser({ username: "", password: "" });
+    setUser({ username: "", password: "", firstName: "", lastName: "" });
   };
 
   const Login = (details: any) => {
@@ -26,6 +33,8 @@ function Login() {
       setUser({
         username: details.username,
         password: details.password,
+        firstName: adminLogin.firstName,
+        lastName: adminLogin.lastName,
       });
     } else {
       console.log("Incorrect Information. Please try again.");
@@ -37,8 +46,11 @@ function Login() {
     <div className="App">
       {user.username != "" ? (
         <>
-          <h1>Dashboard</h1>
-          <SurveyDashboard></SurveyDashboard>
+          <h1>User Information</h1>
+          <p>First Name: {user.firstName}</p>
+          <p>Last Name: {user.lastName}</p>
+          <p>Email: {user.username}</p>
+          <button onClick={Logout}>Logout</button>
         </>
       ) : (
         <LoginForm Login={Login} error={error} />
